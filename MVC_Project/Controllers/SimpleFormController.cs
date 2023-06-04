f7e2422fc7043540224ef52d3ce1d37a86400221
attiq <attiqkhan2@hotmail.com>
2023-06-04 13:55:00 +1200

    First code commit

---
+++ MVC_Project/Controllers/SimpleFormController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC1.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC1.Controllers
+{
+    public class SimpleFormController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SuccessMsg()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MakeJsonFile(SimpleForm simpleForm)
+        {
+            if (ModelState.IsValid)
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "simpleformData.json");
+                var jsonData = JsonSerializer.Serialize(simpleForm);
+                System.IO.File.WriteAllText(filePath, jsonData);
+                TempData["FilePath"] = filePath;
+                return RedirectToAction("SuccessMsg");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
+
---
+++ MVC_Project/Models/SimpleForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace MVC1.Models
+{
+    public class SimpleForm
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter First Name")]
+        public string? firstName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter Last Name")]
+        public string? lastName { get; set; }
+    }
+
+}
+
